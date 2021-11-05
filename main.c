330ae0c00d1508a5b9ae635335a107b7f54e80d5
Samael Chávez <70483436+SamaelCH@users.noreply.github.com>
2021-11-04 21:52:48 -0600

    Update main.c

--- main.c
+++ main.c
@@ -1,20 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#define DOS 2
+#define ZERO 0
 int main(){
-	float x, xx, y, yy, res;
-	printf("inserte el valor de x1:\t");
-	scanf("%f", &x);
-	printf("inserte el valor de x2:\t");
-	scanf("%f", &xx);
-	printf("inserte el valor de y1:\t");
-	scanf("%f", &y);
-	printf("inserte el valor de y2:\t");
-	scanf("%f", &yy);
-	printf("La formula es: dab =  √(x2 - x1)² + (y2 - y1)²\nsustituida es:\t\033[1m√(%f - %f)² + (%f - %f)²\033[0m\n", xx, x, yy, y);
-	float ene = xx - x, eme = yy - y, aa; // esto es un desmadre ayuda
-	aa = pow(ene, 2) + pow(eme, 2);
-	res = sqrt(aa);
-	printf("la distancia entre ambos puntos es:\t%f\n", res);
+	float x, xx, y, yy, m, res;
+	int selection;
+	printf("\033[1mHola y bienvenido\033[0m\nSeleccione la accion que quiera realizar:\n[1]Calcular la distacia de dos puntos\n[2]Calcular la pendiente de dos puntos\n");
+	scanf("%d", &selection);
+	if(selection == 1){
+		printf("Ha seleccionado calcular la distancia de dos puntos\n"); //Aqui empieza el copy-paste
+			printf("inserte el valor de x1:\t");
+				scanf("%f", &x);
+			printf("inserte el valor de x2:\t");
+				scanf("%f", &xx);
+			printf("inserte el valor de y1:\t");
+				scanf("%f", &y);
+			printf("inserte el valor de y2:\t");
+				scanf("%f", &yy);
+			printf("La formula es: dab =  √(x2 - x1)² + (y2 - y1)²\nsustituida es:\t\033[1m√(%f - %f)² + (%f - %f)²\033[0m\n", xx, x, yy, y);
+			float ene = xx - x, eme = yy - y, aa; // esto es un desmadre ayuda
+				aa = pow(ene, DOS) + pow(eme, DOS);
+				res = sqrt(aa);
+			printf("la distancia entre ambos puntos es:\t%f\n", res);
+	}else if(selection == 2){
+			printf("Ha seleccionado calcular la pendiente de dos puntos\n");
+			printf("Inserte el valor de x1:\t");
+				scanf("%f", &x);
+			printf("inserte el valor de x2:\t");
+				scanf("%f", &xx);
+			printf("inserte el valor de y1:\t");
+				scanf("%f", &y);
+			printf("inserte el valor de y2:\t");
+				scanf("%f", &yy);
+			printf("La formula para sacar la pendiente de dos puntos es:\tm = (y2 - y1)/(x2 - x1).\n\033[1mSus\033[0mtituido es:\tm = (%f - %f)/(%f - %f)\n", yy, y, xx, x);
+			float yl, xl;
+			yl = yy - y;
+			xl = xx - x;
+			res = yl/xl;
+			printf("Su respuesta es:\t%f\n", res);
+	}else {
+		printf("Informacion erronea\n");
+	}
+	printf("Gracias por usar\n");
 	return 0;
 }
