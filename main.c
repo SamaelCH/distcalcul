b5e3127865aacb3f92fae599162b8546d0eb8c4d
Samael Chávez <70483436+SamaelCH@users.noreply.github.com>
2021-11-05 18:48:22 -0600

    Update main.c

--- main.c
+++ main.c
@@ -3,10 +3,17 @@
 #include <math.h>
 #define DOS 2
 #define ZERO 0
+/* Este es mi primer programa en C, espero que te guste.
+ * Intento añadir muchas cosas a este programa.
+ * Disfruta.
+ * Mi objetivo es mostrar de lo que soy capaz.
+ * Tambien ayudar a otras personas con esto.
+ * Buenas tardes.
+ * :D */
 int main(){
 	float x, xx, y, yy, m, res;
 	int selection;
-	printf("\033[1mHola y bienvenido\033[0m\nSeleccione la accion que quiera realizar:\n[1]Calcular la distacia de dos puntos\n[2]Calcular la pendiente de dos puntos\n");
+	printf("\033[1mHola y bienvenido\033[0m\nSeleccione la accion que quiera realizar:\n[1]Calcular la distacia de dos puntos\n[2]Calcular la pendiente de dos puntos\n[3]Calcular el punto medio de dos puntos\n");
 	scanf("%d", &selection);
 	if(selection == 1){
 		printf("Ha seleccionado calcular la distancia de dos puntos\n"); //Aqui empieza el copy-paste
@@ -39,8 +46,23 @@ int main(){
 			xl = xx - x;
 			res = yl/xl;
 			printf("Su respuesta es:\t%f\n", res);
+	}else if(selection == 3){
+		printf("Ha seleccionado calcular el punto medio de dos puntos\n");
+		printf("Ingrese el valor de x1:\t");
+			scanf("%f", &x);
+		printf("Ingrese el valor de x2:\t");
+			scanf("%f", &xx);
+		printf("Ingrese el valor de y1\t");
+			scanf("%f", &y);
+		printf("Ingrese el valor de y2:\t");
+			scanf("%f", &yy);
+		printf("La formula para sacar el punto medio de dos puntos es:\nxm = (x1 + x2)/2\ty\t ym = (y1 + y2)/2\nSustituida es:\txm = (%f + %f)/2\ty\tym = (%f + %f)/2\n", x, xx, y, yy);
+		float xxx, yyy;
+		xxx = (x + xx) / DOS;
+		yyy = (y + yy) / DOS;
+		printf("El punto medio es:\t(%f,%f)\n", xxx, yyy);
 	}else {
-		printf("Informacion erronea\n");
+		printf("No insertaste una opcion valida\n");
 	}
 	printf("Gracias por usar\n");
 	return 0;
